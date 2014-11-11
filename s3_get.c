7cb8c54cf83f0fde2f1e2312c354ddd60072075b
DQNEO <dqneoo@gmail.com>
2014-11-11 14:55:24 +0900

    rename variable

--- s3_get.c
+++ s3_get.c
@@ -13,9 +13,9 @@
 #define COUNT_FAILED_MAX 5
 
 typedef struct {
-    char* memory;
+    char* body;
     size_t size;
-} Memory;
+} HTTPResponse;
 
 
 void usage()
@@ -30,22 +30,22 @@ size_t _write_memory_callback(void* ptr, size_t size, size_t nmemb, void* data) 
     }
 
     size_t realsize = size * nmemb;
-    Memory* mem = (Memory*) data;
-    mem->memory = (char*) realloc(mem->memory, mem->size + realsize + 1);
-    if (mem->memory) {
-        memcpy(&(mem->memory[mem->size]), ptr, realsize);
+    HTTPResponse* mem = (HTTPResponse*) data;
+    mem->body = (char*) realloc(mem->body, mem->size + realsize + 1);
+    if (mem->body) {
+        memcpy(&(mem->body[mem->size]), ptr, realsize);
         mem->size += realsize;
-        mem->memory[mem->size] = 0;
+        mem->body[mem->size] = 0;
     }
 
     return realsize;
 }
 
-Memory *http_get_content(const char *url, struct curl_slist *slist) {
+HTTPResponse *http_get_content(const char *url, struct curl_slist *slist) {
 
-    Memory *mem = malloc(sizeof(Memory*));
+    HTTPResponse *mem = malloc(sizeof(HTTPResponse*));
     mem->size = 0;
-    mem->memory = NULL;
+    mem->body = NULL;
     int count_download_failed = 0;
 
     while (count_download_failed < COUNT_FAILED_MAX) {
@@ -59,8 +59,8 @@ Memory *http_get_content(const char *url, struct curl_slist *slist) {
         if (curl_easy_perform(curl) == CURLE_OK) {
             break;
         }
-        free(mem->memory);
-        mem->memory = NULL;
+        free(mem->body);
+        mem->body = NULL;
         count_download_failed++;
     }
 
@@ -87,11 +87,11 @@ int main(int argc, char **argv)
 
     sprintf(url,"https://%s/%s/%s", S3_TOKYO_ENDPOINT, bucket ,key);
 
-    Memory *image_blob = http_get_content(url, NULL);
+    HTTPResponse *image_blob = http_get_content(url, NULL);
     if (!image_blob) {
         return 1;
     }
 
-    fwrite(image_blob->memory, image_blob->size, 1,stdout);
+    fwrite(image_blob->body, image_blob->size, 1,stdout);
     return 0;
 }
